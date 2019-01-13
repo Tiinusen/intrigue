9a30c8c7421b29668549b2daac471bc61da01385
Olle Tiinus <olle@tiinus.com>
2019-01-13 22:10:50 +0100

    WIP

---
+++ src/components/StoryMode.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+let props = [];
+export default {
+  watch: {},
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+<style scoped>
+div {
+  width: 100%;
+  height: 100%;
+}
+</style>
